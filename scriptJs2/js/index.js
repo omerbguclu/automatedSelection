65d39e763b153a89ef061dccaf215ea27e0766d2
Omer Buyukguclu <omerbguclu@gmail.com>
2019-10-07 22:13:25 +0300

    Editing Mode Updated(Not Finished)

--- scriptJs2/js/index.js
+++ scriptJs2/js/index.js
@@ -23,7 +23,7 @@ window.onload = function () {
         var new_Input = "<th id='input" + columnCounter + "'>MaxAmmount" + columnCounter + "</th>";
         var new_ToggleButton = "<th><input type='checkbox' id='toggleButton" + columnCounter + "' checked></th>";
 
-        
+
 
         $("th#addColumn").before(new_Input);
         $("#enabledDisabled").before(new_ToggleButton);
@@ -40,10 +40,10 @@ window.onload = function () {
         var currentRow = $('[id=subProjectRow' + (rowCounter - 1) + ']')
         for (let index = 0; index < columnCounter; index++) {
             var columnState = $("[id=toggleButton" + index + "]").is(":checked");
-            if(columnState){
+            if (columnState) {
                 edited_Input = '<td id="td' + index + '">' + tripleStateButton + '</td>';
-            }else{
-                edited_Input = '<td id="td' + index + '">' + index + '</td>';                
+            } else {
+                edited_Input = '<td id="td' + index + '">' + index + '</td>';
             }
             currentRow.append(edited_Input);
             //console.log(edited_Input);
@@ -92,14 +92,14 @@ window.onload = function () {
                 while (tdCount < columnCounter + 1) {
                     var columnState = $("[id=toggleButton" + tdCount + "]").is(":checked");
                     console.log("[id=toggleButton" + tdCount + "]");
-                    if(columnState){
+                    if (columnState) {
                         console.log("[id=toggleButton" + tdCount + "]");
                         edited_Input = '<td id="td' + (tdCount++) + '">' + tripleStateButton + '</td>';
-                    }else{
+                    } else {
                         console.log("passed2");
-                        edited_Input = '<td id="td' + tdCount + '">' +(tdCount++) + '</td>'
+                        edited_Input = '<td id="td' + tdCount + '">' + (tdCount++) + '</td>'
                     }
-                    
+
                     $(this).append(edited_Input);
                 }
             })
@@ -129,71 +129,110 @@ window.onload = function () {
     };
 
     //function updateColumn(){
-        $(document).on('change', '[id*=toggleButton]', function () {
-            var columnNumberAttribute = $(this).attr('id');
-            var columnNumberRegex = new RegExp(/(\d+)/);
-            var columnNumber = columnNumberRegex.exec(columnNumberAttribute);
-            var columnState = $(this).is(':checked');
-            //console.log(columnState);
+    $(document).on('change', '[id*=toggleButton]', function () {
+        var columnNumberAttribute = $(this).attr('id');
+        var columnNumberRegex = new RegExp(/(\d+)/);
+        var columnNumber = columnNumberRegex.exec(columnNumberAttribute);
+        var columnState = $(this).is(':checked');
+        //console.log(columnState);
 
-            var row = $('#rowCopyorClonetBody > tr');
-            var rowChilds = $('[id*=subProject]');
-            var rowCount = row.length;
-            var tdCount = 0;
+        var row = $('#rowCopyorClonetBody > tr');
+        var rowChilds = $('[id*=subProject]');
+        var rowCount = row.length;
+        var tdCount = 0;
 
-            
 
-            if (rowCount > 1) {
-                rowChilds.each(function () {
-                    tdCount = $(this).children('td').length;
-                    //console.log(tdCount);
-                    if(columnState){
-                        $(this).children('#td' + columnNumber[0] + '').html(tripleStateButton);
-                    }else{
-                        $(this).children('#td' + columnNumber[0] + '').html("sad2");
-                    }
-                });
-            }
-        });
+
+        if (rowCount > 1) {
+            rowChilds.each(function () {
+                tdCount = $(this).children('td').length;
+                //console.log(tdCount);
+                if (columnState) {
+                    $(this).children('#td' + columnNumber[0] + '').html(tripleStateButton);
+                } else {
+                    $(this).children('#td' + columnNumber[0] + '').html("sad2");
+                }
+            });
+        }
+    });
     //};
 
-    $(document).on('click', '[name=labels]', function (){
+    $(document).on('click', '[name=labels]', function () {
 
         var $labels = $(this).parent("div").children("label");
         var $activeButton = $(this);
         var buttonValue = $(this).children("input").val();
 
         var colorClasses = ["btn-danger", "btn-info", "btn-success"];
-        
-        $labels.each(function (){
+
+        $labels.each(function () {
             var innerButtonValue = $(this).children("input").val();
-            if (buttonValue == innerButtonValue){
+            if (buttonValue == innerButtonValue) {
                 $activeButton.removeClass("btn-info").addClass(colorClasses[innerButtonValue]);
-            }else{
+            } else {
                 console.log("notPassed");
                 $(this).removeClass(colorClasses[innerButtonValue]).addClass("btn-info");
             }
         });
-        
+
     });
 
     $(document).on('change', '#enabledDisabledForControl', function () {
         var buttonState = $(this).is(":checked");
-        var $thAtt = $("#StringOrBinary").children("th")
-        //console.log($thAtt.length)
+        var $thAtt = $("#StringOrBinary").children("th");
 
-
-        $thAtt.each(function (param) {
+        $thAtt.each(function () {
             if ($(this).attr("id") != "enabledDisabled" && $(this).attr("id") != "toggleNotChange") {
-                if(buttonState){
-                    $(this).children("div").removeClass("disabled",false);
-                    $("[id*=toggleButton]").attr("disabled",false);
-                }else{
+                if (buttonState) {
+                    $(this).children("div").removeClass("disabled", false);
+                    $("[id*=toggleButton]").attr("disabled", false);
+                } else {
                     $(this).children("div").addClass("disabled", true);
-                    $("[id*=toggleButton]").attr("disabled",true);
+                    $("[id*=toggleButton]").attr("disabled", true);
                 }
             }
         });
+        updataValues(buttonState);
     });
+
+    function updataValues(buttonState) {
+        var $subProjectRows = $("[id*=subProjectRow]");
+        var subProjectRowCount = $subProjectRows.length;
+        var $booleanStringColumns = $("[id*=toggleButton] , #toggleNotChange");
+
+        $booleanStringColumns.each(function () {
+            if ($(this).attr("id") == "toggleNotChange") {
+                $subProjectRows.each(function () {
+                    var currentElement = $(this).find("td:first-child");
+                    console.log(currentElement.attr("id"));
+                    var value = currentElement.html();
+                    if (buttonState) {
+                        currentElement.html('<input class="thVal" type="text" value="' + value + '" />');
+                    } else {
+                        currentElement.html($(".thVal").val());
+                    }
+                });
+            } else {
+                if (!($(this).is(":checked"))) {
+                    var toggleRegex = /(\d+)/.exec($(this).attr("id"));
+                    var toggleCounter = parseInt(toggleRegex[0]) + 1;
+                    //var value = currentElement.html();
+                    $subProjectRows.each(function () {
+                        var currentElement = $(this).find('td:nth-child(' + toggleCounter + ')');
+                        var value = currentElement.html();
+                        if (buttonState) {
+                            currentElement.html('<input class="thVal" type="text" value="' + value + '" />');
+                        } else {
+                            currentElement.html($(".thVal").val());
+                        }
+                    });
+                }
+            }
+        });
+        /*
+
+        */
+
+    }
 
 }
